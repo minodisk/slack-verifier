31b02eccccbffd6bc7c07ca231d3453203ae3fd0
Daisuke Mino <daisuke.mino@gmail.com>
2024-06-20 00:37:05 +0900

    tee

--- src/verifier.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const invalidTimestampError: Error;
-export declare const requestTooOldError: Error;
-export declare const signatureMismatchError: Error;
-declare const _default: (signatureSecret: string) => (req: Request) => Promise<void>;
-export default _default;
